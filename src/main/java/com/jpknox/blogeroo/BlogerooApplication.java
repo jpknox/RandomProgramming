b79c9edc0e03a429c31a022704bd70be3923b8ae
Joao Paulo Knox <joao.knox@crunch.co.uk>
2017-01-23 14:20:58 +0000

    First commit of jay-blogeroo

---
+++ src/main/java/com/jpknox/blogeroo/BlogerooApplication.java
@@ -0,0 +1,12 @@
+package com.jpknox.blogeroo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogerooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogerooApplication.class, args);
+	}
+}
