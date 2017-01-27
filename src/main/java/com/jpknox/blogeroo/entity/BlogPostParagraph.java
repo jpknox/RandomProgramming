a0adb9eb8524267107699ad1181423fb29ddce0c
Joao Paulo Knox <joao.knox@crunch.co.uk>
2017-01-27 08:36:58 +0000

    Added support for larger bodies of texts in paragraphs.

--- src/main/java/com/jpknox/blogeroo/entity/BlogPostParagraph.java
+++ src/main/java/com/jpknox/blogeroo/entity/BlogPostParagraph.java
@@ -1,5 +1,6 @@
 package com.jpknox.blogeroo.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -11,6 +12,7 @@ public class BlogPostParagraph {
 	@Id @GeneratedValue
 	private Long id;
 	
+	@Column( length = 1024)
 	private String text;
 	
 	@ManyToOne
--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -32,3 +32,44 @@ insert into blog_post_paragraph
 insert into blog_post_paragraph
 (id, text, parent_blog_post_id) values
 (6, 'Though, after a while, it becomes very enjoyable.', 2);
+
+
+insert into blog_post
+(id, title, subtitle, author)
+values
+(3, 'Building an Android app with Java', 'Where should I start?', 'Joao Paulo Knox');
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(7, 'Skip the Java bit and go straight to React-Native.', 3);
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(8, 'Only kidding.', 3);
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(9, 'Learn the absolute basics of Android development using Java then move to React-Native', 3);
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(10, 'Doing so will allow a developer to debug the transpiled output with more precision.', 3);
+
+
+insert into blog_post
+(id, title, subtitle, author)
+values
+(4, 'Top Notch Filler Text', 'Hot off the presses', 'Joao Paulo Knox');
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(11, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae laoreet mauris. Sed efficitur, magna id vestibulum aliquet, purus mi venenatis ex, efficitur tristique massa purus id lacus. Proin vel bibendum lorem. Nunc nunc felis, vestibulum in augue ac, suscipit fringilla mauris. Nullam vel faucibus odio, vitae mattis leo. Morbi sit amet gravida justo. Sed ullamcorper et libero vitae molestie. Ut auctor posuere sollicitudin.', 4);
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(12, 'Maecenas mollis commodo lorem ut auctor. Cras neque erat, volutpat sed iaculis vitae, ullamcorper eu nisl. Pellentesque ut ex non augue laoreet laoreet et sit amet nulla. Quisque sed tellus tempor, semper ipsum sit amet, molestie nisl. Suspendisse potenti. Nunc ac metus vel eros dictum tincidunt quis id leo. Quisque volutpat interdum dui blandit lobortis. Integer sagittis mauris a tellus dapibus suscipit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nunc quis metus non nisl dignissim ultricies. Sed augue nunc, suscipit a mi non, congue ultricies ex. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.', 4);
+
+insert into blog_post_paragraph
+(id, text, parent_blog_post_id) values
+(13, 'Vivamus ultrices velit ipsum, vel lacinia ipsum auctor vitae. Nunc condimentum dignissim mauris ac laoreet. Etiam nec dolor et arcu accumsan sollicitudin at vitae lectus. Duis ac aliquet dolor. Nulla commodo, elit eu aliquam convallis, nisl justo feugiat felis, at tempor nisi urna quis turpis. Vivamus commodo semper facilisis. Sed ut justo pellentesque arcu vulputate dignissim vel sit amet enim. Suspendisse a convallis dui. Donec laoreet ligula hendrerit quam luctus, in dictum dui posuere. Morbi vitae placerat orci.', 4);
+
