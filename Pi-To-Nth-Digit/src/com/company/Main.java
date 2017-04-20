6c9aa5ea26f70520a56fe341718d5e650b0833aa
J P Knox <joaoknox@hotmail.com>
2017-04-20 18:03:14 +0100

    Initial commit of my Pi calculator. Recreating various equations using Java.

---
+++ Pi-To-Nth-Digit/src/com/company/Main.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Main {
+
+	private static final long n = 10000000000L;
+
+    public static void main(String[] args) {
+
+        PiCalculator piCalculator = new PiCalculator();
+
+
+
+//       System.out.println(piCalculator.getPi());
+		System.out.println(piCalculator.getPiNilakantha(n));
+		System.out.println("3.1415926535897932");
+
+
+    }
+}
