e88f7df519928ca13b525eab09f7303211ee7389
J P Knox <joaoknox@hotmail.com>
2017-05-01 15:27:40 +0100

    Three buttons. Clicking one sets its text value and resets the text values of the others.

--- JavaFX/MultipleScenes/src/sample/Controller.java
+++ JavaFX/MultipleScenes/src/sample/Controller.java
@@ -9,10 +9,52 @@ public class Controller {
 	@FXML
 	Button nameButton; //The fx:id attribute within the FXML links to the identical name of this button variable.
 
+	@FXML
+	Button townButton;
+
+	@FXML
+	Button boroughButton;
+
 	@FXML  //The @FXML annotation allows this method to be referenced from the FXML within an attribute of an element.
 	private void  handleNameButtonAction(ActionEvent action) {
 		String name = "Joao Paulo Knox";
 		System.out.println(name);
 		nameButton.setText(name);  //Using the @FXML injection above the button is accessed.
+		resetOtherButtonsText((Button) action.getSource());
+	}
+
+	public void handleTownButtonAction(ActionEvent actionEvent) {
+		String town = "Cheltenham";
+		System.out.println(town);
+		townButton.setText(town);
+		resetOtherButtonsText((Button) actionEvent.getSource());
+
+	}
+
+	public void handleBoroughButtonAction(ActionEvent actionEvent) {
+		String borough = "Gloucestershire";
+		System.out.println(borough);
+		boroughButton.setText(borough);
+		resetOtherButtonsText((Button) actionEvent.getSource());
+	}
+
+	private int resetOtherButtonsText(Button callingButton) {
+		String defaultBtnText = "Button";
+		switch (callingButton.getId()) {
+			case "nameButton":
+				townButton.setText(defaultBtnText);
+				boroughButton.setText(defaultBtnText);
+				break;
+			case "townButton":
+				nameButton.setText(defaultBtnText);
+				boroughButton.setText(defaultBtnText);
+				break;
+			case "boroughButton":
+				nameButton.setText(defaultBtnText);
+				townButton.setText(defaultBtnText);
+				break;
+		}
+
+		return 0;
 	}
 }
