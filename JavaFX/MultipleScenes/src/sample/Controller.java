41787c97a138cd42da01114d037c8bba6c910f2b
J P Knox <joaoknox@hotmail.com>
2017-05-01 14:44:21 +0100

    A button, which locates its controller and action-method listener using the FXML.

--- JavaFX/MultipleScenes/src/sample/Controller.java
+++ JavaFX/MultipleScenes/src/sample/Controller.java
@@ -1,4 +1,17 @@
 package sample;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class Controller {
+
+	@FXML
+	Button nameButton;
+
+
+
+	public void notifyNameAction(ActionEvent actionEvent) {
+		System.out.println("Joao Paulo Knox");
+	}
 }
