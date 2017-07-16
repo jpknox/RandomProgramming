4c26500a46817ca96efbbe66c5456d816d289b92
J P Knox <joaoknox@hotmail.com>
2017-07-16 13:56:17 +0100

    Access System properties/Environment variables. Create folders and files.

---
+++ files/src/sample/Main.java
@@ -0,0 +1,53 @@
+package sample;
+
+import java.io.*;
+import java.util.Map;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        Map<String, String> envMap = System.getenv();
+        printMap(envMap, "Environment variables");
+
+        System.getProperties().list(System.out);
+
+        System.out.println("Create file");
+        getTxtFile();
+        writeToFile(getTxtFile());
+    }
+
+    private static void printMap(Map<String, String> mapToPrint, String descriptor) {
+        System.out.println(descriptor);
+        int i = 1;
+        for (Map.Entry<String, String> envEntry: mapToPrint.entrySet()) {
+            System.out.println(i++ + ": " + envEntry.getKey() + "/" + envEntry.getValue());
+        }
+        System.out.println();
+    }
+
+    private static File getAppDirectory() {
+        return new File(System.getProperty("user.dir"));
+    }
+
+    private static File getSubFolder() {
+        File subFolder = new File(getAppDirectory(), "subFolder");
+        subFolder.mkdir();
+        if(subFolder.exists()) return subFolder;
+        else return null;
+    }
+
+    private static File getTxtFile() throws IOException {
+        File textFile = new File(getSubFolder(), "textFile.txt");
+        textFile.createNewFile();
+        if(textFile.exists()) return textFile;
+        else return null;
+    }
+
+    private static void writeToFile(File writeTo) throws FileNotFoundException, UnsupportedEncodingException {
+        PrintWriter printWriter = new PrintWriter(writeTo, "UTF-8");
+        printWriter.println("First line.");
+        printWriter.println("Second line.");
+        printWriter.close();
+    }
+}
+
