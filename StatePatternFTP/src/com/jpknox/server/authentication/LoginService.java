7ab449959c91cf61a2d72272c45e87419b002061
J P Knox <joaoknox@hotmail.com>
2017-10-14 13:16:38 +0100

    REFACTOR - Removed unnecessary parentheses

--- StatePatternFTP/src/com/jpknox/server/authentication/LoginService.java
+++ StatePatternFTP/src/com/jpknox/server/authentication/LoginService.java
@@ -20,24 +20,24 @@ public class LoginService {
                 context.setState(new StateLoggedIn());
                 log("Username \"" + username + "\" logged in.");
                 context.setClientName(username);    //TODO: what if the client changes, and fails to login?
-                return ("230 " + firstCharUpper(username) + " logged in, proceed.");
+                return "230 " + firstCharUpper(username) + " logged in, proceed.";
             } else {
                 context.setState(new StateNeedPassword(username));
-                return ("331 User name okay, need password.");
+                return "331 User name okay, need password.";
             }
         } else {
             context.setState(new StateNotLoggedIn());
-            return ("530 Not logged in.");
+            return "530 Not logged in.";
         }
     }
 
     public String login(ClientSession context, String username, String password) {
         if (loginAuthentication.authenticate(username, password)) {
             context.setState(new StateLoggedIn());
-            return ("230 "+ firstCharUpper(username) + " logged in, proceed.");
+            return "230 "+ firstCharUpper(username) + " logged in, proceed.";
         } else {
             context.setState(new StateNotLoggedIn());
-            return ("530 Not logged in.");
+            return "530 Not logged in.";
         }
     }
 
