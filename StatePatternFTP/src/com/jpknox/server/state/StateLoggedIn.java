d6ac377aaf122a5017032565ec7c80ca0bc48a1c
J P Knox <joaoknox@hotmail.com>
2017-12-23 14:27:57 +0000

    REFACTOR Removed all redundant code within every state
    The abstract superclass will now diseminate all repeated code.

--- StatePatternFTP/src/com/jpknox/server/state/StateLoggedIn.java
+++ StatePatternFTP/src/com/jpknox/server/state/StateLoggedIn.java
@@ -1,78 +1,7 @@
 package com.jpknox.server.state;
 
-import com.jpknox.server.utility.FTPServerConfig;
-import com.jpknox.server.utility.FileManager;
-import com.jpknox.server.authentication.LoginService;
-import com.jpknox.server.session.ClientSession;
-
-import static com.jpknox.server.utility.Logger.log;
-
 /**
  * Created by joaok on 24/09/2017.
  */
 public class StateLoggedIn extends AbstractSessionState {
-
-    @Override
-    public String quit(ClientSession session) {
-        return "221 Service closing control connection.";
-    }
-
-    @Override
-    public String port(ClientSession session, int portToUse) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String type(ClientSession session, String format) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String mode(ClientSession session, String modeToUse) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String stru(ClientSession session, String structureToUse) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String retr(ClientSession session, String pathToFile) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String stor(ClientSession session, String pathToFile) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String noop(ClientSession session) {
-        return "200 Command okay.";
-    }
-
-    @Override
-    public String auth(ClientSession session) {
-        log(session.getClientName() + ": 502 command not implemented.");
-        return "502 Command not implemented.";
-    }
-
-    @Override
-    public String syst(ClientSession session) {
-        log(session.getClientName() + ": 215 " + config.OPERATING_SYSTEM + ": " + config.SERVER_NAME);
-        return "215 " + config.OPERATING_SYSTEM + ": " + config.SERVER_NAME;
-    }
-
-    @Override
-    public String feat(ClientSession session) {
-        log(session.getClientName() + ": 502 FEAT command not implemented.");
-        return "502 Command not implemented.";
-    }
-
-    @Override
-    public String pwd(ClientSession session) {
-        log(session.getClientName() + ": 257 " + fileManager.getCurrentDirectory());
-        return "257 " + fileManager.getCurrentDirectory();
-    }
 }
--- StatePatternFTP/src/com/jpknox/server/state/StateNeedPassword.java
+++ StatePatternFTP/src/com/jpknox/server/state/StateNeedPassword.java
@@ -1,12 +1,7 @@
 package com.jpknox.server.state;
 
-import com.jpknox.server.utility.FTPServerConfig;
-import com.jpknox.server.utility.FileManager;
 import com.jpknox.server.authentication.LoginAuthentication;
-import com.jpknox.server.authentication.LoginService;
 import com.jpknox.server.session.ClientSession;
-
-import static com.jpknox.server.utility.Logger.log;
 
 /**
  * Created by joaok on 03/10/2017.
@@ -24,69 +19,5 @@ public class StateNeedPassword extends AbstractSessionState {
     @Override
     public String pass(ClientSession session, String password) {
         return loginService.login(session, this.username, password);
-    }
-
-    @Override
-    public String quit(ClientSession session) {
-        return "221 Service closing control connection.";
-    }
-
-    @Override
-    public String port(ClientSession session, int portToUse) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String type(ClientSession session, String format) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String mode(ClientSession session, String modeToUse) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String stru(ClientSession session, String structureToUse) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String retr(ClientSession session, String pathToFile) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String stor(ClientSession session, String pathToFile) {
-        return "530 Not logged in.";
-    }
-
-    @Override
-    public String noop(ClientSession session) {
-        return "200 Command okay.";
-    }
-
-    @Override
-    public String auth(ClientSession session) {
-        log(session.getClientName() + ": 502 command not implemented.");
-        return "502 Command not implemented.";
-    }
-
-    @Override
-    public String syst(ClientSession session) {
-        log(session.getClientName() + ": 215 " + config.OPERATING_SYSTEM + ": " + config.SERVER_NAME);
-        return "215 " + config.OPERATING_SYSTEM + ": " + config.SERVER_NAME;
-    }
-
-    @Override
-    public String feat(ClientSession session) {
-        log(session.getClientName() + ": 502 FEAT command not implemented.");
-        return "502 Command not implemented.";
-    }
-
-    @Override
-    public String pwd(ClientSession session) {
-        log(session.getClientName() + ": 257 " + fileManager.getCurrentDirectory());
-        return "257 " + fileManager.getCurrentDirectory();
     }
 }
